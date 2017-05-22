c32cf82c2eaff996255a5970bd52cb0f21cd0c42
psicho <9400878@gmail.com>
2017-05-22 19:26:34 +0400

    Доработаны параметры запросов.

--- rings/rings/spiders/rings.py
+++ rings/rings/spiders/rings.py
@@ -3,25 +3,29 @@ from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.contrib.loader.processor import TakeFirst
 from scrapy.contrib.loader import XPathItemLoader
 from scrapy.selector import HtmlXPathSelector
-from jewels.items import JewelsItem
+from rings.items import RingsItem
 
 
-class JewelsLoader(XPathItemLoader):
+class RingsLoader(XPathItemLoader):
     default_output_processor = TakeFirst()
 
-class JewelSpader(CrawlSpider):
+class RingsSpader(CrawlSpider):
     name = "jewels"
     allowed_domains = ["rings.su"]
     start_urls = ["http://rings.su/catalog"]
 
     rules = (
-        Rule(SgmlLinkExtractor(allow=('act=catalog')), follow=True),
-        Rule(SgmlLinkExtractor(allow=('act=goods')), callback='parse_item'),
+        Rule(SgmlLinkExtractor(allow=('catalog.+')), follow=True),
+        Rule(SgmlLinkExtractor(allow=('goods.+')), callback='parse_item'),
     )
 
     def parse_item(self, response):
         hxs = HtmlXPathSelector(response)
-        l = JewelsLoader(JewelsItem(), hxs)
-        l.add_xpath('id', "//td[text()='%s']/following-sibling::td/text()" % u"Рег. номер:")
+        l = RingsLoader(RingsItem(), hxs)
+        l.add_xpath('name', "<h1 itemprop=\"name\">(.+)<\/h1>")
+        # l.add_xpath('text', "<h1 itemprop=\"name\">(.+)<\/h1>")
+
+
         l.add_value('url', response.url)
+
         return l.load_item()
